4c417a5a9ad9004b3fdbc17277c505402127a627
unknown <you@example.com>
2020-06-11 19:25:39 +0300

    Сделал кнопку js адаптировал под SE navbar & footer

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.menu-block');
+menuButton.addEventListener('click', function () {
+	menu.classList.toggle('menu-block-active');	
+})
