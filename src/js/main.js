785792eb34b287975c24f1819ffa29010e9083a1
unknown <you@example.com>
2020-06-29 15:11:50 +0300

    Отформатировал код

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 var menuButton = document.querySelector('.menu-button');
 var menu = document.querySelector('.menu-block');
 menuButton.addEventListener('click', function () {
-	menu.classList.toggle('menu-block-active');	
+	menu.classList.toggle('menu-block-active');
 })
